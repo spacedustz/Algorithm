39d2da2530e060d56195517d494a4862c7b60108
spacedustz <spacedustw@gmail.com>
2024-02-26 20:59:37 +0900

    이항 계수 - DP & 재귀로 풀이

--- src/backjoon/class2/Baek_11050.java
+++ src/backjoon/class2/Baek_11050.java
@@ -5,18 +5,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/* 11050 이항 계수 1 */
+/**
+ * 11050 이항 계수
+ * nCr = n! / ((n-r)! * r!) 공식
+ * 입력값 : 5 2
+ */
 public class Baek_11050 {
     static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken()); // 5
+        int b = Integer.parseInt(st.nextToken()); // 2
 
         dp = new int[a+1];
+
+        // dp[0], dp[1]은 Factorial 기본값인 1로 초기화
         dp[0]=1;
         dp[1]=1;
+
         System.out.println(factorial(a) / (factorial(a-b) * factorial(b)));
         br.close();
     }
